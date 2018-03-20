b026553300b365c41e5ea4aae9edef294867daf1
Mokretsov, Oleksandr <OMokretsov@webmd.net>
2018-03-20 11:27:12 -0400

    Initial Commit

---
+++ UserDefaultsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserDefaultsSample
+//
+//  Created by Mokretsov, Oleksandr on 3/20/18.
+//  Copyright © 2018 Smart Logic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserDefaultsSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserDefaultsSample
+//
+//  Created by Mokretsov, Oleksandr on 3/20/18.
+//  Copyright © 2018 Smart Logic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
